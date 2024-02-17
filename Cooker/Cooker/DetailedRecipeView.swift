48374e8714e3af4b94c8d522a6eb5e26a6ab1051
aarav-nair <aarav.nair@gmail.com>
2024-02-17 13:16:59 -0500

    Detailed

---
+++ Cooker/Cooker/DetailedRecipeView.swift
@@ -0,0 +1,51 @@
+//
+//  DetailedRecipeView.swift
+//  Cooker
+//
+//  Created by Aarav Nair on 2/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct DetailedRecipeView: View {
+    var recipe: Recipe
+    
+    var body: some View {
+        VStack {
+            Text(recipe.recipeName)
+                .font(.title)
+            Image(recipe.imageName)
+                .resizable()
+                .frame(width: 150, height: 150)
+            
+            Text("Cook Time: \(recipe.cookTime)")
+                .font(.subheadline)
+            if (recipe.funFact != nil) {
+                Text("Fun Fact: " + recipe.funFact!)
+                    .font(.subheadline)
+            }
+            
+            Spacer()
+            Text("Ingredients:")
+                .font(.title2)
+            ForEach(recipe.ingredients, id: \.self) { ingredient in
+                Text("• " + ingredient)
+                    .font(.body)
+            }
+            
+            Spacer()
+            Text("Instructions:")
+                .font(.title2)
+            ForEach(recipe.instructions.indices) { i in
+                Text("\(i+1). \(recipe.instructions[i])")
+            }
+        }
+    }
+}
+
+#Preview {
+    let rec = Recipe(imageName: "cheesepizza", recipeName: "Cheese Pizza", cookTime: 12, cuisine: "Italian", dietaryPreferences: ["Vegetarian"], ingredients: ["Dough", "Cheese", "Sauce"], instructions: ["Spread the pizza sauce onto the dough.",  "Sprinkle with cheeses.", "Bake for 12 to 14 minutes."])
+    return DetailedRecipeView(recipe: rec)
+}
+    
