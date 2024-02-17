7485a932bbd041c6a68bccfb25d339b0fc5dae87
Yongye Tan <yongyetan1209@gmail.com>
2024-02-17 13:16:26 -0500

    add my stuff

--- Cooker/Cooker/MyRecipesView.swift
+++ Cooker/Cooker/MyRecipesView.swift
@@ -28,6 +28,6 @@ struct MyRecipesView: View {
 }
     
 #Preview {
-    let rec = Recipe(imageName: "cheesepizza", recipeName: "Cheese Pizza", cookTime: 12, cuisine: "Italian", dietaryPreferences: ["Vegetarian"], ingredients: ["Dough", "Cheese", "Sauce"], instructions: ["Spread the pizza sauce onto the dough.",  "Sprinkle with cheeses.", "Bake for 12 to 14 minutes."])
+    let rec = Recipe(id: 1, imageName: "cheesepizza", recipeName: "Cheese Pizza", cookTime: 12, cuisine: "Italian", dietaryPreferences: ["Vegetarian"], ingredients: ["Dough", "Cheese", "Sauce"], instructions: ["Spread the pizza sauce onto the dough.",  "Sprinkle with cheeses.", "Bake for 12 to 14 minutes."])
     return MyRecipesView(recipeArray: [rec])
 }
