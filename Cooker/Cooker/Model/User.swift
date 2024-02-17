7620fac55ca6ce6b8bbcf38d3fb49a96ab19c2df
Yongye Tan <yongyetan1209@gmail.com>
2024-02-17 11:54:51 -0500

    add receipt and user struct

---
+++ Cooker/Cooker/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Cooker
+//
+//  Created by Yongye Tan on 2/17/24.
+//
+
+import Foundation
+
+struct User {
+    var dietaryPreferences: [String]
+    var cuisines: [String]
+}
