df8e928e7ef3a07e8876dd2042531b5ead189fd8
Yongye Tan <yongyetan1209@gmail.com>
2024-02-17 18:56:59 -0500

    update videoModel

--- Cooker/Cooker/CookViewModel.swift
+++ Cooker/Cooker/CookViewModel.swift
@@ -12,5 +12,13 @@ import Foundation
     var selectedPreferences: [String] = []
     var selectedCuisines: [String] = []
     
-    init() { }
+    var selectedRecipe: [Recipe] = []
+    
+    var allRecipes: [Recipe] = []
+    
+    init() {
+        allRecipes = [
+            Recipe(id: 1, imageName: "cheesepizza", recipeName: "1", cookTime: 30, cuisine: "USA", dietaryPreferences: ["no milk"], ingredients: ["apple"], instructions: ["cook", "clean", "eat"]),
+            Recipe(id: 2, imageName: "food", recipeName: "1", cookTime: 30, cuisine: "USA", dietaryPreferences: ["no milk"], ingredients: ["apple"], instructions: ["cook", "clean", "eat"], funFact: "A dialog is a type of modal window that appears in front of app content to provide critical information, or prompt for a decision to be made.")]
+    }
 }
