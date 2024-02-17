0a7ef1a6d2d58e925ede5144acb4b05ed6e4ee86
aarav-nair <aarav.nair@gmail.com>
2024-02-17 12:27:07 -0500

    Added All Recipes View

---
+++ Cooker/Cooker/MyRecipesView.swift
@@ -0,0 +1,33 @@
+//
+//  MyRecipesView.swift
+//  Cooker
+//
+//  Created by Aarav Nair on 2/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct MyRecipesView: View {
+    var recipeArray: [Recipe]
+    
+    var body: some View {
+        VStack {
+            Text("My Recipes")
+                .font(.title)
+            ForEach(recipeArray, id: \.self) { recipe in
+                Button {
+                    
+                } label: {
+                    Image(systemName: recipe.imageName)
+                }
+            }
+            .padding()
+        }
+    }
+}
+    
+#Preview {
+    let rec = Recipe(imageName: "cheesepizza", recipeName: "Cheese Pizza", cookTime: 12, cuisine: "Italian", dietaryPreferences: ["Vegetarian"], ingredients: ["Dough", "Cheese", "Sauce"], instructions: ["Spread the pizza sauce onto the dough.",  "Sprinkle with cheeses.", "Bake for 12 to 14 minutes."])
+    return MyRecipesView(recipeArray: [rec])
+}
